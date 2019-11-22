597b039affed97e19574df8e391630be1ddfcfd9
Richard Libre <meng@codinggirl.net>
2019-11-22 16:50:36 +0800

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
-import './Navbar.css';
+import React from 'react'
+import './Navbar.css'
 
-class Navbar extends Component {
-    render() {
-        return (
-            <div className="topnav">
-                <a href="https://join.slack.com/t/firstcontributors/shared_invite/enQtNjkxNzQwNzA2MTMwLTVhMWJjNjg2ODRlNWZhNjIzYjgwNDIyZWYwZjhjYTQ4OTBjMWM0MmFhZDUxNzBiYzczMGNiYzcxNjkzZDZlMDM"  target="_blank" rel="noopener noreferrer">Slack</a>
-                <a href="https://www.youtube.com/channel/UCMXNFxCvyH5LhUwEcmY8qGQ" target="_blank" rel="noopener noreferrer">YouTube</a>
-                <a href="https://twitter.com/1stContribution" target="_blank" rel="noopener noreferrer">Twitter</a>
-                <a href="https://github.com/firstcontributions/first-contributions" target="_blank" rel="noopener noreferrer">GitHub</a>
-            </div>
-        );
-    }
+function Navbar() {
+    return (
+        <div className="topnav">
+            <a href="https://github.com/1stcontribution/1stcontribution" rel="noopener">1st contribution</a>
+            <a href="https://github.com/1stcontribution/1stcontribution-web" rel="noopener">web</a>
+        </div>
+    )
 }
 
-export default Navbar;
+export default Navbar
